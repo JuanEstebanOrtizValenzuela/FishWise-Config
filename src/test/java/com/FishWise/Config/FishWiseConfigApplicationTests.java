613d258b7f548118a4825067b65afc148b4f1467
JuanOrtiz <juan_ortizva@fet.edu.co>
2024-05-05 15:28:02 -0500

    dev env

---
+++ src/test/java/com/FishWise/Config/FishWiseConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FishWise.Config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishWiseConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
